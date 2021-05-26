db92e832f6f0fe8f09c651d3b9e3f7de5c2ab5fa
DeonJ76 <deon.jordaan76@gmail.com>
2021-05-26 22:46:17 +0200

    Attempting parallax scroll

--- script.js
+++ script.js
@@ -32,6 +32,43 @@ document.addEventListener('DOMContentLoaded', init);
 
 // Init
 function init() {
+	const introCode = [
+		'type() {',
+		'let timer;',
+		'const fullText = this.phrase;',
+
+		'this.text = fullText.substring(0, this.text.length + 1);',
+
+		'this.textElement.classList.add("cursor");',
+
+		'this.textElement.innerHTML = `<span>${this.text}</span>`;',
+
+		'let typeSpeed = 100;',
+
+		'timer = setTimeout(() => this.type(), typeSpeed);',
+
+		'if (this.text.length === this.phrase.length) {',
+		'clearTimeout(timer);',
+		'this.textElement.classList.remove("cursor");',
+		'}',
+		'}',
+		'}',
+	];
+
+	// const introOverlay = function () {
+	// 	let codeElement = [];
+	// 	let codeSnippet = [];
+	// 	for (let i = 1; i < 15; i++) {
+	// 		eval(
+	// 			`const code${i} = document.querySelector('.code__snippet--${i}')`;
+	// 			);
+	// 		eval
+
+	// 			const codeSnippet${i} = code${i}.getAttribute('data-text-${i}');
+	// 			new TypeWriter(code[i], codeSnippet[i])`
+	// 	}
+	// };
+
 	const code1 = document.querySelector('.code__snippet--1');
 	const code2 = document.querySelector('.code__snippet--2');
 	const code3 = document.querySelector('.code__snippet--3');
@@ -47,35 +84,41 @@ function init() {
 	const code13 = document.querySelector('.code__snippet--13');
 	const code14 = document.querySelector('.code__snippet--14');
 
-	const codeSnippet1 = code1.getAttribute('data-text-1');
-	const codeSnippet2 = code2.getAttribute('data-text-2');
-	const codeSnippet3 = code3.getAttribute('data-text-3');
-	const codeSnippet4 = code4.getAttribute('data-text-4');
-	const codeSnippet5 = code5.getAttribute('data-text-5');
-	const codeSnippet6 = code6.getAttribute('data-text-6');
-	const codeSnippet7 = code7.getAttribute('data-text-7');
-	const codeSnippet8 = code8.getAttribute('data-text-8');
-	const codeSnippet9 = code9.getAttribute('data-text-9');
-	const codeSnippet10 = code10.getAttribute('data-text-10');
-	const codeSnippet11 = code11.getAttribute('data-text-11');
-	const codeSnippet12 = code12.getAttribute('data-text-12');
-	const codeSnippet13 = code13.getAttribute('data-text-13');
-	const codeSnippet14 = code14.getAttribute('data-text-14');
+	// const introOverlay = function () {
+	// 	for (let i = 1; i < 15; i++) {
+	// 		new TypeWriter(code1, introCode[i]);
+	// 	}
+	// };
 
-	new TypeWriter(code1, codeSnippet1);
-	new TypeWriter(code2, codeSnippet2);
-	new TypeWriter(code3, codeSnippet3);
-	new TypeWriter(code4, codeSnippet4);
-	new TypeWriter(code5, codeSnippet5);
-	new TypeWriter(code6, codeSnippet6);
-	new TypeWriter(code7, codeSnippet7);
-	new TypeWriter(code8, codeSnippet8);
-	new TypeWriter(code9, codeSnippet9);
-	new TypeWriter(code10, codeSnippet10);
-	new TypeWriter(code11, codeSnippet11);
-	new TypeWriter(code12, codeSnippet12);
-	new TypeWriter(code13, codeSnippet13);
-	new TypeWriter(code14, codeSnippet14);
+	// const codeSnippet1 = code1.getAttribute('data-text-1');
+	// const codeSnippet2 = code2.getAttribute('data-text-2');
+	// const codeSnippet3 = code3.getAttribute('data-text-3');
+	// const codeSnippet4 = code4.getAttribute('data-text-4');
+	// const codeSnippet5 = code5.getAttribute('data-text-5');
+	// const codeSnippet6 = code6.getAttribute('data-text-6');
+	// const codeSnippet7 = code7.getAttribute('data-text-7');
+	// const codeSnippet8 = code8.getAttribute('data-text-8');
+	// const codeSnippet9 = code9.getAttribute('data-text-9');
+	// const codeSnippet10 = code10.getAttribute('data-text-10');
+	// const codeSnippet11 = code11.getAttribute('data-text-11');
+	// const codeSnippet12 = code12.getAttribute('data-text-12');
+	// const codeSnippet13 = code13.getAttribute('data-text-13');
+	// const codeSnippet14 = code14.getAttribute('data-text-14');
+
+	// new TypeWriter(code1, codeSnippet1);
+	// new TypeWriter(code2, codeSnippet2);
+	// new TypeWriter(code3, codeSnippet3);
+	// new TypeWriter(code4, codeSnippet4);
+	// new TypeWriter(code5, codeSnippet5);
+	// new TypeWriter(code6, codeSnippet6);
+	// new TypeWriter(code7, codeSnippet7);
+	// new TypeWriter(code8, codeSnippet8);
+	// new TypeWriter(code9, codeSnippet9);
+	// new TypeWriter(code10, codeSnippet10);
+	// new TypeWriter(code11, codeSnippet11);
+	// new TypeWriter(code12, codeSnippet12);
+	// new TypeWriter(code13, codeSnippet13);
+	// new TypeWriter(code14, codeSnippet14);
 
 	const greetingElement = document.querySelector('.welcome-text--main');
 	const greeting = greetingElement.getAttribute('data-textGreet');
@@ -85,4 +128,5 @@ function init() {
 	setTimeout(function () {
 		new TypeWriter(introElement, introduction);
 	}, 2000);
+	// introOverlay();
 }
