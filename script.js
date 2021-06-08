f1f34cf7bdeae6ace9508056060f203d91a171b7
DeonJ76 <deon.jordaan76@gmail.com>
2021-06-08 10:09:28 +0200

    Images resized and gallery functionality updated

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const galleryImages = document.querySelectorAll('.gallery__item');
-const galleryCarousel = document.querySelector('.gallery__carousel');
+const galleryCarousel = document.getElementById('carousel');
+
+const leftButton = document.getElementById('left');
+const closeButton = document.getElementById('close');
+const rightButton = document.getElementById('right');
 
 const showCarousel = function () {
-	galleryCarousel.style.visibility = 'visible';
+	galleryCarousel.classList.toggle('hidden');
+	// if (galleryCarousel.className === 'hidden') {
+	// 	galleryCarousel.className !== 'hidden';
+	// } else {
+	// 	galleryCarousel.className === 'hidden';
+	// }
+	// if (el.className === 'active'){
+	// 	el.className = 'inactive';
+	//   } else {
+	// 	el.className = 'active';
+	//   }
 };
 
 galleryImages.forEach((item) => item.addEventListener('click', showCarousel));
+
+closeButton.addEventListener('click', showCarousel);
