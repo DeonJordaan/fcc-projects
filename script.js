0160bfd4a9c615e7ec524dc80467b01a6a3d5d87
DeonJ76 <deon.jordaan76@gmail.com>
2021-06-08 18:05:15 +0200

    Getting drunk on the image-carousel

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const galleryImages = document.querySelectorAll('.gallery__item');
 const galleryCarousel = document.getElementById('carousel');
+const carouselOverlay = document.querySelector('.carousel__overlay');
+const carouselImages = document.querySelector('.carousel__container');
+const buttonContainer = document.querySelector('.buttons-container');
 
 const leftButton = document.getElementById('left');
 const closeButton = document.getElementById('close');
@@ -7,6 +10,9 @@ const rightButton = document.getElementById('right');
 
 const showCarousel = function () {
 	galleryCarousel.classList.toggle('hidden');
+	carouselOverlay.classList.toggle('hidden');
+	buttonContainer.classList.toggle('hidden');
+
 	// if (galleryCarousel.className === 'hidden') {
 	// 	galleryCarousel.className !== 'hidden';
 	// } else {
@@ -22,3 +28,25 @@ const showCarousel = function () {
 galleryImages.forEach((item) => item.addEventListener('click', showCarousel));
 
 closeButton.addEventListener('click', showCarousel);
+
+let index = 0;
+
+function changeImage() {
+	if (index > carouselImages.length - 1) {
+		index = 0;
+	} else if (index < 0) {
+		index = carouselImages.length - 1;
+	}
+
+	carouselImages.style.transform = `translateX(${-index * 500}px)`;
+}
+
+rightButton.addEventListener('click', () => {
+	index++;
+	changeImage();
+});
+
+leftButton.addEventListener('click', () => {
+	index--;
+	changeImage();
+});
